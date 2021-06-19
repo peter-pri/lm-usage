9c95d75b0bf506be9e91f655a8f08ba7524cc8cc
peter-pri <PeterPrisil@gmail.com>
2021-06-19 10:20:32 +0200

    cleanup print statements

--- plot_candlestick_chart.py
+++ plot_candlestick_chart.py
@@ -29,16 +29,13 @@ df = pd.DataFrame(columns=column_names, index=range(length))
 
 for i in range(len(parsed['results'])):
     date_p = arrow.get(parsed['results'][i]['t']).datetime
-    print(date_p)
     df.loc[length - 1 - i]["Date"] = date_p
     df.loc[length - 1 - i]["Open"] = parsed['results'][i]['o']
     df.loc[length - 1 - i]["High"] = parsed['results'][i]['h']
     df.loc[length - 1 - i]["Low"] = parsed['results'][i]['l']
     df.loc[length - 1 - i]["Close"] = parsed['results'][i]['c']
     df.loc[length - 1 - i]["Volume"] = 0
-    print(length - 1 - i)
 
-print("df =", df)
 df.Date = pd.to_datetime(df.Date)
 df.Open = pd.to_numeric(df.Open)
 df.High = pd.to_numeric(df.High)
@@ -46,6 +43,7 @@ df.Low = pd.to_numeric(df.Low)
 df.Close = pd.to_numeric(df.Close)
 df.Volume = pd.to_numeric(df.Volume)
 df = df.set_index('Date')
+print("df =", df)
 print("df.info() =", df.info())
 mpf.plot(df, type='candle', tight_layout=False, style="yahoo")
 mpf.show()
