0e890eb29bc0942a1682338dce3c687e3ea5c1c2
peter-pri <PeterPrisil@gmail.com>
2021-06-24 12:47:42 +0200

    Initial version

---
+++ Candlestick_Interactive.py
@@ -0,0 +1,63 @@
+# Plot a Interactive Candlestick Chart in a Browser
+import requests
+import json
+import arrow
+import pandas as pd
+import plotly.graph_objects as go
+
+isin = "US67066G1040"  # NVIDIA
+
+mic = "XMUN"
+
+X1 = "d1"  # Specify the type of data you want: m1, h1, or d1
+
+TOKEN_KEY = "insert_your_token_here"  # please adapt to your token !
+
+authorization = f"Bearer {TOKEN_KEY}"
+
+request = requests.get(f"https://paper.lemon.markets/rest/v1/trading-venues/{mic}/instruments/{isin}/data/ohlc/{X1}/",
+                       headers={"Authorization": authorization})
+
+print(request)
+parsed = json.loads(request.content)
+print(json.dumps(parsed, indent=4, sort_keys=True))
+length = len(parsed['results'])
+
+column_names = ["Date", "Open", "High", "Low", "Close", "Volume"]
+df = pd.DataFrame(columns=column_names, index=range(length))
+
+for i in range(len(parsed['results'])):
+    date_p = arrow.get(parsed['results'][i]['t']).datetime
+    df.loc[length - 1 - i]["Date"] = date_p
+    df.loc[length - 1 - i]["Open"] = parsed['results'][i]['o']
+    df.loc[length - 1 - i]["High"] = parsed['results'][i]['h']
+    df.loc[length - 1 - i]["Low"] = parsed['results'][i]['l']
+    df.loc[length - 1 - i]["Close"] = parsed['results'][i]['c']
+    df.loc[length - 1 - i]["Volume"] = 0
+
+df.Date = pd.to_datetime(df.Date)
+df.Open = pd.to_numeric(df.Open)
+df.High = pd.to_numeric(df.High)
+df.Low = pd.to_numeric(df.Low)
+df.Close = pd.to_numeric(df.Close)
+df.Volume = pd.to_numeric(df.Volume)
+df = df.set_index('Date')
+print("df =", df)
+print("df.info() =", df.info())
+
+figure = go.Figure(
+    data=[
+        go.Candlestick(
+            x=df.index,
+            low=df["Low"],
+            high=df["High"],
+            open=df["Open"],
+            close=df["Close"]
+        )
+    ]
+)
+
+# This open a new browser tab !
+figure.show()
+
+
