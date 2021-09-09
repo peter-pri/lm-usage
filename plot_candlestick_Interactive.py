be5bb61c162e8eca04fce74ae799d6e63dc4b43d
peter-pri <PeterPrisil@gmail.com>
2021-09-09 10:14:49 +0200

    Adapt to new API-Version

--- plot_candlestick_Interactive.py
+++ plot_candlestick_Interactive.py
@@ -4,20 +4,37 @@ import json
 import arrow
 import pandas as pd
 import plotly.graph_objects as go
+from datetime import timedelta
+import datetime
+import time
+
+DELTA_OF_31_DAYS = timedelta(days=31)
+DELTA_OF_7_DAYS = timedelta(days=7)
 
 isin = "US67066G1040"  # NVIDIA
 
 mic = "XMUN"
 
-X1 = "d1"  # Specify the type of data you want: m1, h1, or d1
+time_step = "h1"  # Specify the type of data you want: m1, h1, or d1
 
 TOKEN_KEY = "insert_your_token_here"  # please adapt to your token !
 
 authorization = f"Bearer {TOKEN_KEY}"
 
-request = requests.get(f"https://paper.lemon.markets/rest/v1/trading-venues/{mic}/instruments/{isin}/data/ohlc/{X1}/",
-                       headers={"Authorization": authorization})
+datetime_today = datetime.datetime.today()
+start_time = datetime_today - DELTA_OF_7_DAYS
 
+# Do no request old data which are not available
+if start_time < datetime.datetime(2021, 8, 1, 0, 0):
+    start_time = datetime.datetime(2021, 8, 1, 0, 0)
+
+start_time_str = str(int((time.mktime(start_time.timetuple())) * 1000))
+
+end_time = datetime_today
+
+end_time_str = str(int((time.mktime(end_time.timetuple())) * 1000))
+request = requests.get(f"https://paper-data.lemon.markets/v1/ohlc/{time_step}/?mic={mic}&isin={isin}&to={end_time_str}&from={start_time_str}&epoch=True",
+                       headers={"Authorization": authorization})
 print(request)
 parsed = json.loads(request.content)
 print(json.dumps(parsed, indent=4, sort_keys=True))
@@ -27,14 +44,12 @@ column_names = ["Date", "Open", "High", "Low", "Close", "Volume"]
 df = pd.DataFrame(columns=column_names, index=range(length))
 
 for i in range(len(parsed['results'])):
-    date_p = arrow.get(parsed['results'][i]['t']).datetime
-    df.loc[length - 1 - i]["Date"] = date_p
-    df.loc[length - 1 - i]["Open"] = parsed['results'][i]['o']
-    df.loc[length - 1 - i]["High"] = parsed['results'][i]['h']
-    df.loc[length - 1 - i]["Low"] = parsed['results'][i]['l']
-    df.loc[length - 1 - i]["Close"] = parsed['results'][i]['c']
-    df.loc[length - 1 - i]["Volume"] = 0
-
+    df.loc[i]["Date"] = arrow.get(parsed['results'][i]['t']).to('local').datetime
+    df.loc[i]["Open"] = parsed['results'][i]['o']
+    df.loc[i]["High"] = parsed['results'][i]['h']
+    df.loc[i]["Low"] = parsed['results'][i]['l']
+    df.loc[i]["Close"] = parsed['results'][i]['c']
+    df.loc[i]["Volume"] = 0
 df.Date = pd.to_datetime(df.Date)
 df.Open = pd.to_numeric(df.Open)
 df.High = pd.to_numeric(df.High)
@@ -42,6 +57,7 @@ df.Low = pd.to_numeric(df.Low)
 df.Close = pd.to_numeric(df.Close)
 df.Volume = pd.to_numeric(df.Volume)
 df = df.set_index('Date')
+
 print("df =", df)
 print("df.info() =", df.info())
 
@@ -55,6 +71,12 @@ figure = go.Figure(
             close=df["Close"]
         )
     ]
+)
+
+figure.update_layout(
+    title='Stock: ' + isin,
+    yaxis_title='EUR',
+    xaxis_title='Local Date and Time',
 )
 
 # This open a new browser tab !
